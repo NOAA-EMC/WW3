958f61988d798b96c9f4ff8e8da462fb2d381c8e
ardhuin@ifremer.fr <ardhuin@ifremer.fr>
2011-08-29 08:13:33 +0000

    Addition of limiter in k-space advection and correction of boundary bugs in unstructured grids. This includes a clean-up of w3triamd.ftn and w3profsmd.ftn
    Former-commit-id: ebafae2fc9145959f0d9f973d23298d94ed6d75b

--- model/bin/make_makefile.sh
+++ model/bin/make_makefile.sh
@@ -84,7 +84,7 @@
   for type in mach nco grib shared mpp thr0 thr1 c90 nec lrecl grid \
               prop stress s_ln source stab s_nl s_bot s_db s_tr s_bs s_xx \
               wind windx rwind curr currx tdyn dss0 pdif miche \
-              mgwind mgprop mggse nnt mprf reflection mcp
+              mgwind mgprop mggse nnt mprf reflection mcp netcdf
   do
     case $type in
       mach   ) TY='one'
@@ -213,9 +213,13 @@
                TS='REF1'
                OK='REF1' ;;
       mcp    ) TY='one'
-               ID='model coupling protocol'
+               ID='model as program / subroutine'
                TS='NOPA'
                OK='NOPA PALM' ;;
+      netcdf ) TY='upto1'
+               ID='model coupling protocol'
+               TS='NC0'
+               OK='NC3 NC4' ;;
     esac
 
     n_found='0'
@@ -293,9 +297,12 @@
       s_xx   ) s_xx=$sw ;;
       reflection    ) reflection=$sw ;;
       mcp    ) mcp=$sw ;;
+      netcdf ) nclib=$sw;;
         *    ) ;;
     esac
   done
+
+echo "NCLIB .............................:" $nclib 
 
   if [ "$nr_thr" != '0' ] && [ "$shared" != 'SHRD' ]
   then
@@ -467,9 +474,8 @@
 
 # 2.c Make makefile and file list  - - - - - - - - - - - - - - - - - - - - - -
 
-  progs='ww3_grid ww3_strt ww3_prep ww3_prnc ww3_shel ww3_multi ww3_sbs1
-         ww3_outf ww3_outp ww3_trck ww3_grib gx_outf gx_outp ww3_ounf 
-         ww3_ounp ww3_gint'
+  progs='ww3_grid ww3_strt ww3_prep ww3_shel ww3_multi ww3_sbs1
+         ww3_outf ww3_outp ww3_trck ww3_grib gx_outf gx_outp ww3_gint'
 
   for prog in $progs
   do
@@ -489,13 +495,6 @@
                  IO='w3iogrmd w3iorsmd'
                 aux='constants w3servmd w3arrymd w3dispmd w3gsrumd' ;;
      ww3_prep) IDstring='Field preprocessor'
-               core='w3fldsmd'
-               data='w3gdatmd w3adatmd w3idatmd w3odatmd'
-               prop=
-             source="w3triamd $stx $nlx $btx"
-                 IO='w3iogrmd'
-                aux='constants w3servmd w3timemd w3arrymd w3dispmd w3gsrumd' ;;
-     ww3_prnc) IDstring='NetCDF field preprocessor'
                core='w3fldsmd'
                data='w3gdatmd w3adatmd w3idatmd w3odatmd'
                prop=
@@ -537,21 +536,7 @@
              source="$stx $nlx $btx"
                  IO='w3iogrmd w3iogomd'
                 aux='constants w3servmd w3timemd w3arrymd w3dispmd w3gsrumd' ;;
-     ww3_ounf) IDstring='Gridded NetCDF output'
-               core=
-               data='w3gdatmd w3wdatmd w3adatmd w3idatmd w3odatmd'
-               prop=
-             source="w3triamd $stx $nlx $btx"
-                 IO='w3iogrmd w3iogomd'
-                aux='constants w3servmd w3timemd w3arrymd w3dispmd w3gsrumd' ;;
      ww3_outp) IDstring='Point output'
-               core=
-               data='w3triamd w3gdatmd w3wdatmd w3adatmd w3idatmd w3odatmd'
-               prop=
-             source="$flx $ln $st $nl $bt $db $tr $bs $xx"
-                 IO='w3bullmd w3iogrmd w3iopomd w3partmd'
-                aux='constants w3servmd w3timemd w3arrymd w3dispmd w3gsrumd' ;;
-     ww3_ounp) IDstring='Point NetCDF output'
                core=
                data='w3triamd w3gdatmd w3wdatmd w3adatmd w3idatmd w3odatmd'
                prop=
@@ -594,7 +579,6 @@
                  IO='w3iogrmd w3iopomd'
                 aux='constants w3servmd w3timemd w3arrymd w3dispmd w3gsrumd' ;;
     esac
-
     d_string='$(aPe)/'"$prog"' : $(aPo)/'
     files="$aux $core $data $prop $source $IO $prog"
     filesl="$prog $data $core $prop $source $IO $aux"
@@ -610,6 +594,54 @@
     echo ' '                                     >> makefile
   done
 
+#
+# Programs with NetCDF libraries
+#
+  if [ "$nclib" != '' ] 
+  then 
+    progs='ww3_prnc ww3_ounf ww3_ounp'
+
+    for prog in $progs
+    do
+      case $prog in
+        ww3_prnc) IDstring='NetCDF field preprocessor'
+               core='w3fldsmd'
+               data='w3gdatmd w3adatmd w3idatmd w3odatmd'
+               prop=
+             source="w3triamd $stx $nlx $btx"
+                 IO='w3iogrmd'
+                aux='constants w3servmd w3timemd w3arrymd w3dispmd w3gsrumd' ;;
+        ww3_ounf) IDstring='Gridded NetCDF output'
+               core=
+               data='w3gdatmd w3wdatmd w3adatmd w3idatmd w3odatmd'
+               prop=
+             source="w3triamd $stx $nlx $btx"
+                 IO='w3iogrmd w3iogomd'
+                aux='constants w3servmd w3timemd w3arrymd w3dispmd w3gsrumd' ;;
+        ww3_ounp) IDstring='Point NetCDF output'
+               core=
+               data='w3triamd w3gdatmd w3wdatmd w3adatmd w3idatmd w3odatmd'
+               prop=
+             source="$flx $ln $st $nl $bt $db $tr $bs $xx"
+                 IO='w3bullmd w3iogrmd w3iopomd w3partmd'
+                aux='constants w3servmd w3timemd w3arrymd w3dispmd w3gsrumd' ;;
+      esac
+
+      d_string='$(aPe)/'"$prog"' : $(aPo)/'
+      files="$aux $core $data $prop $source $IO $prog"
+      filesl="$prog $data $core $prop $source $IO $aux"
+
+      echo "# $IDstring"                           >> makefile
+      echo ' '                                     >> makefile
+      for file in $files
+      do
+        echo "$d_string$file.o"                    >> makefile
+        echo "$file"                               >> filelist.tmp
+      done
+      echo '	@$(aPb)/link '"$filesl"          >> makefile
+      echo ' '                                     >> makefile
+    done  
+  fi
   sort -u filelist.tmp                            > filelist
   rm -f filelist.tmp
 
