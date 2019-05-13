6ec05d8b19081286ead3b438662d5157c830a614
Henrique.Alves <henrique.alves@noaa.gov>
2019-05-13 21:18:30 +0000

    Branch esmf_makefile: Updating files

--- model/esmf/esmf_stdalone_make.sh
+++ model/esmf/esmf_stdalone_make.sh
@@ -1,6 +1,18 @@
- module use ../../../NEMS/src/conf 
+#!/bin/bash
+
+# Temporary script to compile esmf codes for testing modifications
+
+ NEMSdir=$1 # Requires FV3-WW3 coupled app installed in machine
+
+ if [ -z $NEMSdir ]
+ then
+  echo " Variable NEMSdir not set "
+  echo " Usage: ./esmf_stdalone_make.sh NEMSdir"  
+  exit
+ fi
+ module use ${NEMSdir}/NEMS/src/conf 
  module load modules.nems              ; module list   
- ulimit -S -s $stack ; set -x ; cd /scratch4/NCEPDEV/ocean/noscrub/Henrique.Alves/EMC_FV3-WW3/WW3/model/esmf
+ ulimit -S -s $stack ; set -x ; #cd `pwd`
  export COMP_SRCDIR="/scratch4/NCEPDEV/ocean/noscrub/Henrique.Alves/EMC_FV3-WW3/WW3/model" COMP_BINDIR="/scratch4/NCEPDEV/ocean/noscrub/Henrique.Alves/EMC_FV3-WW3/WW3/WW3_INSTALL" WW3_COMP="theia"
  set -e
  exec make -j 1 WW3_COMP="theia" ww3_nems 1> make.out 2>&1
