bfda7150889898bec842fecd3b8d033be91065ce
Henrique Alves <henrique.alves@noaa.gov>
2019-05-16 19:10:40 +0000

    Branch esmf_makefile: modifications to support using intel option and generalized cmplr.env comp/link framework for building WW3 esmf for NEMS application

--- model/esmf/esmf_stdalone_make.sh
+++ model/esmf/esmf_stdalone_make.sh
@@ -2,8 +2,8 @@
 
 # Temporary script to compile esmf codes for testing modifications
 
- NEMSdir=$1 # Requires to enter location of NEMS dir
- WW3dir=$1 # Requires to enter location of WW3 model codes
+ NEMSdir=${NEMSdir:-$1} # Requires to enter location of NEMS dir
+ WW3dir=${WW3dir:-$1} # Requires to enter location of WW3 model codes
 
  if [ -z $NEMSdir ] || [ -z $WW3dir ]
  then
@@ -11,10 +11,17 @@
   echo " Usage: ./esmf_stdalone_make.sh NEMSdir WW3dir"  
   exit
  fi
- module use ${NEMSdir}/NEMS/src/conf 
- module load modules.nems              ; module list   
+
+ hostl=`hostname | cut -c1`
+ if [ "$hostl" = "v" ] || [ "$hostl" = "m" ]
+ then
+   machn="wcoss_dell_p3"
+ fi
+
+ module use ${NEMSdir}/modulefiles/${machn}
+ module load fv3ww3              ; module list   
  ulimit -S -s $stack ; set -x ; #cd `pwd`
- export COMP_SRCDIR="${WW3dir}/model" COMP_BINDIR="${WW3dir}/model/bin" WW3_COMP="theia"
+ export COMP_SRCDIR="${WW3dir}/model" COMP_BINDIR="${WW3dir}/model/bin" WW3_COMP="intel"
  set -e
- exec make -j 1 WW3_COMP="theia" ww3_nems 1> make.out 2>&1
+ exec make -j 1 WW3_COMP="intel" ww3_nems 1> make.out 2>&1
 
