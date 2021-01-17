4dfafdbed48f42b3c7314693c55be758b5743c51
Jessica Meixner <jessica.meixner@noaa.gov>
2021-01-17 11:28:20 -0500

    removing comp.* link.* files as we now use the cmplr.env (#284)
    * removing file that should not be in repository
    
    * adding to .gitignore to avoid future .*.swp files
    
    * remove unused files from aux directory
    
    * removing comp.* link.* files as we now use the cmplr.env

--- model/aux/idl_twist/make_visu_tar.sh
+++
@@ -1,6 +0,0 @@
-tar -cvf rt_visumain.tar  visu.pro initmain.pro rt_visumain.pro read_timeseries.pro julday.pro mapping.pro gridbuild.pro  bathytool.pro editspecials.pro maketransect.pro raytracer.pro parametros.pro   palette.pro xloadpalette.pro  
-tar -uvf rt_visumain.tar palettes/*
-tar -uvf rt_visumain.tar rayp.f90  raypfc.f90  raypf.f90  raypg.f90 
-tar -uvf rt_visumain.tar TWIST_init.txt
-gzip rt_visumain.tar
-
