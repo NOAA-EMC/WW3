ef2da974c322fab7ea243289627bc3a474729b46
Ali <ali.abdolali@noaa.gov>
2021-03-18 14:54:27 +0000

    fix for mww3_04 link with SCRIP and SCRIPNC switch

--- model/bin/make_makefile.sh
+++ model/bin/make_makefile.sh
@@ -893,8 +893,11 @@
                 aux="$aux scrip_constants scrip_grids scrip_iounitsmod"
                 aux="$aux scrip_remap_vars scrip_timers scrip_errormod scrip_interface"
                 aux="$aux scrip_kindsmod scrip_remap_conservative wmscrpmd" 
+                fi 
+              if [ "$scripnc" = 'SCRIPNC' ]
+                then
+                  aux="$aux scrip_netcdfmod scrip_remap_write scrip_remap_read"
                 fi ;;
-
      ww3_strt) IDstring='Initial conditions program'
                core=
                data="$memcode w3gdatmd w3wdatmd w3adatmd w3idatmd w3odatmd"
