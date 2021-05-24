4b91840f65ccbcb4a73a806138bd7dd397b711b9
Qingxiang Liu <qingxiangl@4000l-145556-l.mobility.unimelb.net.au>
2021-05-24 13:29:16 +0800

    FB_IC5: Issue #72 - Updates of regtest/ww3_tic1.1 for input_IC5_M* (24/05/2021)

--- ww3_env.sh
+++
@@ -1,5 +0,0 @@
-# WW3 master for developing
-export PATH=$PATH:~/WW3/model/bin
-export PATH=$PATH:~/WW3/model/exe
-export WWATCH3_NETCDF=NC4
-export NETCDF_CONFIG=~/Mysoft/netcdf-4.1.3-gnu/bin/nc-config
