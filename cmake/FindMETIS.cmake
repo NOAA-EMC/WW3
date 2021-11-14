32be750ad0a3fb901b6efbf17daa286cf76b60cc
kgerheiser <kgerheiser@icloud.com>
2021-11-13 21:26:05 -0500

    Add FindMETIS/FindParMETIS modules

---
+++ cmake/FindMETIS.cmake
@@ -0,0 +1,20 @@
+find_library(metis_lib NAMES libmetis.a libmetis.so libmetis.dylib HINTS ENV METIS_PATH PATH_SUFFIXES lib)
+find_path(metis_inc metis.h HINTS ENV METIS_PATH PATH_SUFFIXES include)
+
+find_package(METIS REQUIRED)
+
+target_link_libraries(ParMETIS::ParMETIS PUBLIC METIS::METIS)
+
+add_library(METIS::METIS STATIC IMPORTED)
+
+set_target_properties(METIS::METIS PROPERTIES
+  IMPORTED_LOCATION "${metis_lib}"
+  INTERFACE_INCLUDE_DIRECTORIES "${metis_inc}")
+
+## Finalize find_package
+include(FindPackageHandleStandardArgs)
+
+find_package_handle_standard_args(
+    ${CMAKE_FIND_PACKAGE_NAME}
+    REQUIRED_VARS metis_lib
+                  metis_inc)
