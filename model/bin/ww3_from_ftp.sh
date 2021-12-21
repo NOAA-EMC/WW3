dbb5ae75cf96cbb756ce0126e28a250fad085da7
ma05cde <mickael.accensi@ifremer.fr>
2021-12-21 14:20:33 +0000

    add option --no-check-certificate to download archive

--- model/bin/ww3_from_ftp.sh
+++ model/bin/ww3_from_ftp.sh
@@ -22,7 +22,7 @@ cd $ww3dir
 
 #Download from ftp and uptar: 
 echo -e "Downloading and untaring file from ftp:" 
-wget https://ftp.emc.ncep.noaa.gov/static_files/public/WW3/ww3_from_ftp.${ww3ver}.tar.gz
+wget --no-check-certificate https://ftp.emc.ncep.noaa.gov/static_files/public/WW3/ww3_from_ftp.${ww3ver}.tar.gz
 tar -xvzf ww3_from_ftp.${ww3ver}.tar.gz
 
 #Move regtest info from data_regtests to regtests:
