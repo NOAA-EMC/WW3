dda08fe9555d737f25521cff2d4350734d301d1d
Denise Worthen <denise.worthen@noaa.gov>
2022-02-17 08:13:07 -0500

    add switch UWM
    * add switch UWM for resetting charn in w3updtdmd for restart
    repro

--- model/src/w3iogoncdmd.F90
+++ model/src/w3iogoncdmd.F90
@@ -676,7 +676,7 @@ contains
                    endif
                 endif !NC
 
-             end if ! end of if ( FLOGRD(IFI,IFJ) ) 
+             end if ! end of if ( FLOGRD(IFI,IFJ) )
           end do IFJ_LOOP
        end do IFI_LOOP
     end do NC_LOOP
@@ -732,7 +732,7 @@ contains
 
 !/ ------------------------------------------------------------------- /
   SUBROUTINE HANDLE_ERR(IERR,STRING)
-    USE W3ODATMD, ONLY: NDSE 
+    USE W3ODATMD, ONLY: NDSE
     USE W3SERVMD, ONLY: EXTCDE
     USE NETCDF
 
@@ -742,7 +742,7 @@ contains
     integer         ,intent(in) :: ierr
     character(len=*),intent(in) :: string
 
-    IF (IERR /= NF90_NOERR) then 
+    IF (IERR /= NF90_NOERR) then
          WRITE(NDSE,*) "*** WAVEWATCH III netCDF error: ",trim(string),':',trim(nf90_strerror(IERR))
          CALL EXTCDE ( 49 )
     END IF
