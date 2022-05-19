feb333dd60ff4bd8a7b9d04ed6d6d7b4a00f58b7
Mickael Accensi <49198861+mickaelaccensi@users.noreply.github.com>
2022-05-19 16:41:28 +0200

    Feature/nml (#698)
    update all the nml files in regtests and keep only the header of each namelist

--- model/tools/bash/ww3_bounc_inp2nml.sh
+++ model/tools/bash/ww3_bounc_inp2nml.sh
@@ -2,13 +2,16 @@
 
 prog="ww3_bounc"
 
-if [ $# -ne 1 ]
+if [ $# -ne 2 ]
 then
-  echo "  [ERROR] need ${prog} input filename in argument [${prog}.inp]"
+  echo '[ERROR] need 2 arguments : '
+  echo "\$1 : ${prog} input filename in argument [${prog}.inp]"
+  echo '$2 : include header or full comments [header|full]' 
   exit 1
 fi
 
 inp="$( cd "$( dirname "$1" )" && pwd )/$(basename $1)"
+comment="$2"
 
 # check filename extension
 ext=$(echo $inp | awk -F '.' '{print $NF}')
@@ -16,18 +19,6 @@ if [ "$(echo $ext)" != 'inp' ] ; then
   echo "[ERROR] input file has no .inp extension. Please rename it before conversion"  
   exit 1
 fi
-
-# commented because it is not working in all cases
-# link to temporary inp with regtest format
-#ext=$(echo $inp | awk -F"${prog}.inp." '{print $2}' || awk -F"${prog}.inp_" '{print $2}')
-#base=$(echo $inp | awk -F"${prog}\\..inp\\.." '{print $1}' | awk -F".inp.$ext" '{print $1}' || awk -F"${prog}\\..inp_" '{print $1}' | awk -F".inp_$ext" #'{print $1}')
-#if [ ! -z $(echo $ext) ] ; then
-# new_inp=${base}_${ext}.inp
-# echo "link $inp to $new_inp"
-# ln -sfn $inp $new_inp
-# old_inp=$inp
-# inp=$new_inp
-#fi
 
 cd $( dirname $inp)
 cur_dir="../$(basename $(dirname $inp))"
@@ -57,7 +48,7 @@ do
     continue
   fi
 
-  cleanline="$(echo $line | awk -F' ' '{print $1}' | cut -d \" -f2  | cut -d \' -f2)"  
+  cleanline="$(echo $line | awk -F' ' '{print $1}' | sed -e "s/\*//g" -e "s/\"//g" -e "s/\'//g")"  
   if [ -z "$cleanline" ]
   then
     continue
@@ -73,6 +64,13 @@ done
 # get all values from clean inp file
 
 readarray -t lines < "$cleaninp"
+numlines=${#lines[@]}
+
+# remove carriage return characters
+for il in $(seq 0 $numlines)
+do
+  lines[$il]=$(echo "$(echo ${lines[$il]} | sed 's/\r$//')")
+done
 il=0
 
 mode="$(echo ${lines[$il]} | awk -F' ' '{print $1}' | cut -d \" -f2  | cut -d \' -f2)"
@@ -132,6 +130,7 @@ cat > $nmlfile << EOF
 EOF
 
 # field namelist
+if [ "$comment" = "full" ]; then
 cat >> $nmlfile << EOF
 ! -------------------------------------------------------------------- !
 ! Define the input boundaries to preprocess via BOUND_NML namelist
@@ -145,6 +144,14 @@ cat >> $nmlfile << EOF
 ! -------------------------------------------------------------------- !
 &BOUND_NML
 EOF
+else
+cat >> $nmlfile << EOF
+! -------------------------------------------------------------------- !
+! Define the input boundaries to preprocess via BOUND_NML namelist
+! -------------------------------------------------------------------- !
+&BOUND_NML
+EOF
+fi
 
 if [ "${mode}" != "WRITE" ];  then            echo "  BOUND%MODE        =  '${mode}'" >> $nmlfile; fi
 if [ "$interp" != "2" ];  then                echo "  BOUND%INTERP      =  $interp" >> $nmlfile; fi
@@ -160,15 +167,6 @@ cat >> $nmlfile << EOF
 EOF
 echo "DONE : $( cd "$( dirname "$nmlfile" )" && pwd )/$(basename $nmlfile)"
 rm -f $cleaninp
-
-# commented because it is not working in all cases
-#if [ ! -z $(echo $ext) ] ; then
-#  unlink $new_inp
-#  addon="$(echo $(basename $nmlfile) | awk -F"${prog}_" '{print $2}' | awk -F'.nml' '{print $1}'  )"
-#  new_nmlfile="${prog}.nml.$addon"
-#  mv $( cd "$( dirname "$nmlfile" )" && pwd )/$(basename $nmlfile) $( cd "$( dirname "$nmlfile" )" && pwd )/$(basename $new_nmlfile)
-#  echo "RENAMED  : $( cd "$( dirname "$nmlfile" )" && pwd )/$(basename $new_nmlfile)"
-#fi
 
 #------------------------------
 
