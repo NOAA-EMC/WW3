59da952438678fb9a48e694a11332c5de413d7f2
Denise Worthen <denise.worthen@noaa.gov>
2022-11-08 10:25:31 -0500

    remove trailing whitespace from pr 841

--- model/src/w3updtmd.F90
+++ model/src/w3updtmd.F90
@@ -1,6 +1,6 @@
-!> @file 
+!> @file
 !> @brief Bundles all input updating routines for WAVEWATCH III.
-!> 
+!>
 !> @author H. L. Tolman
 !> @date   22-Mar-2021
 !>
@@ -10,10 +10,10 @@
 
 !>
 !> @brief Bundles all input updating routines for WAVEWATCH III.
-!> 
+!>
 !> @author H. L. Tolman
 !> @date   22-Mar-2021
-!> 
+!>
 !> @copyright Copyright 2009-2022 National Weather Service (NWS),
 !>       National Oceanic and Atmospheric Administration.  All rights
 !>       reserved.  WAVEWATCH III is a trademark of the NWS.
@@ -475,7 +475,7 @@ CONTAINS
   !>
   !> @brief Interpolate wind fields to the given time.
   !>
-  !> @details Linear interpolation of wind speed and direction, with a 
+  !> @details Linear interpolation of wind speed and direction, with a
   !>  simple correction to obtain quasi-conservation of energy.
   !>
   !> @param[in] FLFRST  Flag for first pass through routine.
@@ -484,7 +484,7 @@ CONTAINS
   !>
   !> @author H. L. Tolman
   !> @date   27-May-2014
-  !>  
+  !>
   SUBROUTINE W3UWND ( FLFRST, VGX, VGY )
     !/
     !/                  +-----------------------------------+
@@ -1034,7 +1034,7 @@ CONTAINS
   !>
   !> @brief Initialize the wave field with fetch-limited spectra before
   !>  the actual calculation start.
-  !>     
+  !>
   !>
   !> @details Named as an update routine due to placement in code.
   !>
@@ -1045,7 +1045,7 @@ CONTAINS
   !>
   !> @author H. L. Tolman
   !> @date   06-Jun-2018
-  !>  
+  !>
   SUBROUTINE W3UINI ( A )
     !/
     !/                  +-----------------------------------+
@@ -1611,11 +1611,11 @@ CONTAINS
     !/
   END SUBROUTINE W3UIC1
   !/ ------------------------------------------------------------------- /
-  
+
   !> @attention FLFRST not currently used.
   !> @brief Update ice floe mean and max diameters in the wave model.
   !>
-  !> @param[in] FLFRST  
+  !> @param[in] FLFRST
   !>
   !> @author C. Sevigny
   !> @author F. Ardhuin
@@ -1735,12 +1735,12 @@ CONTAINS
     !/
   END SUBROUTINE W3UIC5
   !/ ------------------------------------------------------------------- /
-  
+
 !>
 !> @brief Update ice map in the wave model.
 !>
 !> @details Points with an ice concentration larger than FICEN are
-!>  removed from the sea map in the wave model. Such points are 
+!>  removed from the sea map in the wave model. Such points are
 !>  identified by negative numbers is the grid status map MAPSTA. For
 !>  ice points spectra are set to zero. Points from which ice disappears
 !>  are initialized with a "small" JONSWAP spectrum, based on the
@@ -1994,7 +1994,7 @@ CONTAINS
   !>
   !> @brief Update the water level.
   !>
-  !> @details The wavenumber grid is modified without modyfying the 
+  !> @details The wavenumber grid is modified without modyfying the
   !>  spectrum (conservative linear interpolation to new grid).
   !>
   !> @param[inout] A   2-D represetation of the spectra.
@@ -2538,7 +2538,7 @@ CONTAINS
   !>
   !> @author J. M. Castillo
   !> @date   13-Aug-2021
-  !>  
+  !>
   SUBROUTINE W3URHO ( FLFRST )
     !/
     !/                  +-----------------------------------+
@@ -2718,11 +2718,11 @@ CONTAINS
   !>  for all other cases (by definition full transparency).
   !>
   !> @param[inout] TRNX  Transparencies from model definition file.
-  !> @param[inout] TRNY  Transparencies from model definition file.  
+  !> @param[inout] TRNY  Transparencies from model definition file.
   !>
   !> @author H. L. Tolman
   !> @date   30-Oct-2009
-  !>  
+  !>
   SUBROUTINE W3UTRN ( TRNX, TRNY )
     !/
     !/                  +-----------------------------------+
@@ -3115,7 +3115,7 @@ CONTAINS
   !> @brief Calculate derivatives of a field.
   !>
   !> @details Derivatives are calculated in m/m from the longitude/latitude
-  !>  grid, central in space for iternal points, one-sided for coastal 
+  !>  grid, central in space for iternal points, one-sided for coastal
   !>  points.
   !>
   !> @param[in]  ZZ     Field to calculate derivatives of.
@@ -3125,7 +3125,7 @@ CONTAINS
   !>
   !> @author W. E. Rogers, NRL
   !> @date   06-Dec-2010
-  !>  
+  !>
   SUBROUTINE W3DZXY( ZZ, ZUNIT, DZZDX, DZZDY )
     !/
     !/                  +-----------------------------------+
