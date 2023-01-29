4e080fb2680dc88ba44ae79f970702690c680ded
Aron Roland <aaronroland@gmx.de>
2023-01-29 18:24:19 +0000

    ww3_wise_merge_develop: turn of new limiter

--- model/src/w3srcemd.F90
+++ model/src/w3srcemd.F90
@@ -811,7 +811,7 @@ CONTAINS
     REAL                    :: eInc1, eInc2, eVS, eVD, JAC
     REAL                    :: DeltaSRC(NSPEC)
     REAL, PARAMETER         :: DTMINTOT = 0.01
-    LOGICAL                 :: LNEWLIMITER = .FALSE.
+    LOGICAL                 :: LNEWLIMITER = .TRUE.
 #ifdef W3_PDLIB
     REAL                 :: PreVS, FAK, DVS, SIDT, FAKS, MAXDAC
 #endif
