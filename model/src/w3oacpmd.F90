4bda7c6caa8a453057f019998b200e7d178dc97b
Aron Roland <aaronroland@gmx.de>
2023-02-24 09:35:23 +0100

    ww3_wise_merge_develop: work on headers

--- model/src/w3oacpmd.F90
+++ model/src/w3oacpmd.F90
@@ -374,8 +374,10 @@ CONTAINS
     !/                  |           A. Thevenin             |
     !/                  |           V. Garnier              |
     !/                  |           M. Accensi              |
+    !/                  !           A. Roland               |
+    !/                  !           H. Michaud              |
     !/                  |                        FORTRAN 90 |
-    !/                  | Last update :         08-Jun-2018 |
+    !/                  | Last update :         22-Feb-2023 |
     !/                  +-----------------------------------+
     !/
     !/      Jul-2013 : Origination.                         ( version 4.18 )
@@ -383,6 +385,7 @@ CONTAINS
     !/                   (R. Baraille & J. Pianezze)
     !/    April-2016 : Add comments (J. Pianezze)           ( version 5.07 )
     !/    08-Jun-2018 : use INIT_GET_ISEA                   ( version 6.04 )
+    !/    22-Feb-2023 : Extend to domain decomposition      ( version 7.xx ) 
     !/
     !  1. Purpose :
     !
