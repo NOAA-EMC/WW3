dbe2e434ed1636d78b2f74dc6422d7650136c1ff
Edward Hartnett <edward.hartnett@noaa.gov>
2023-10-14 04:05:12 -0600

    adding first test

---
+++ model/tests/test_io.F90
@@ -0,0 +1,10 @@
+! This is a test for model IO for WW3.
+!
+! Ed Hartnett 10/14/23
+program test_io
+  implicit none
+
+  print *, 'Testing WW3 IO...'
+  print *, 'SUCCESS!'
+end program test_io
+  
