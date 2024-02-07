3840eaeafb755d6c2d01074282dc43b8d78ce108
Matthew Masarik <Matthew.Masarik@noaa.gov>
2024-02-07 22:31:34 +0000

    src_list.cmake: remove cap-related source files

--- model/src/cmake/src_list.cmake
+++ model/src/cmake/src_list.cmake
@@ -55,23 +55,8 @@ set(ftn_src
   wmupdtmd.F90
   wmwavemd.F90
   w3tidemd.F90
-  wav_grdout.F90
-  w3iogoncdmd.F90
-  wav_shr_flags.F90
   )
 
-set(nuopc_mesh_cap_src
-  wav_kind_mod.F90
-  wav_shr_mod.F90
-  wav_shel_inp.F90
-  wav_comp_nuopc.F90
-  wav_import_export.F90
-  wav_wrapper_mod.F90
-  )
-
-set(esmf_multi_cap_src
-  wmesmfmd.F90
-  )
 
 # Built when PDLIB is enabled
 set(pdlib_src
