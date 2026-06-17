a45360d3ad6dccd6cf6ffd29551cb080818c6f50
Stefan Zieger <stefan.zieger@bom.gov.au>
2026-06-17 15:16:36 +1000

    Undo last commit

--- model/bin/ww3_from_ftp.sh
+++ model/bin/ww3_from_ftp.sh
@@ -66,7 +66,7 @@ echo -e "Downloading (or finding) and untaring file from ftp:"
 if ! test -f ww3_from_ftp.${ww3ver}.tar.gz; then
   wget --no-check-certificate https://ftp.emc.ncep.noaa.gov/static_files/public/WW3/ww3_from_ftp.${ww3ver}.tar.gz
 fi
-tar -xvf --gzip ww3_from_ftp.${ww3ver}.tar.gz
+tar -xvzf ww3_from_ftp.${ww3ver}.tar.gz
 
 
 #Move regtest info from data_regtests to regtests:
